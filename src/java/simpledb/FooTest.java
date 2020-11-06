528e864ccaa6cfbb0fd66236face12f808a8f9db
SikaiTao <Sk_Tao@163.com>
2020-11-06 19:29:55 +0800

    Lab 2--All Finished

--- src/java/simpledb/FooTest.java
+++ src/java/simpledb/FooTest.java
@@ -3,12 +3,6 @@ import java.util.*;
 
 public class FooTest {
     public static void main(String[] args){
-        Map map = new HashMap();
-        map.put(null, null);
-//        map.put(null, null);
-        map.put("3", null);
-        System.out.println(map.get(null));
-        System.out.println(map.size());
 
     }
 }
